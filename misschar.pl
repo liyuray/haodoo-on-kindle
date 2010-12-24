9e0444618db9a090b0be77ab8b510af6dec55c24
yj lee <liyuray@gmail.com>
2010-12-24 14:27:12 +0800

    force call stack dump when die/warning

--- misschar.pl
+++ misschar.pl
@@ -1,10 +1,18 @@
 use strict;
 use warnings;
+$SIG{__DIE__}  = sub { Carp::confess(@_) };
+$SIG{__WARN__} = sub { Carp::cluck(@_) };
 
 binmode STDOUT, ":utf8";
 
+my %hash;
+
 while (<>) {
 	next unless /input str: <(....)>/;
-	my $code = hex($1);
-	print "$code, ",chr($code), $/;
+	my $code = $1;
+	$hash{$code}++;
+}
+
+for my $code (sort {$hash{$b} <=> $hash{$a}} keys %hash) {
+	print "$code $hash{$code}", chr(hex($code)),$/;
 }
